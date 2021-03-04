40524522187345789f0dd2e176aab708ce938f5e
Amogh S Gopadi <amoghsgopadi@gmail.com>
2021-03-04 07:35:24 +0530

    Add files via upload

--- python_scripts/camera.py
+++ python_scripts/camera.py
@@ -1,16 +1,8 @@
 import cv2
 from model import FacialExpressionModel
 import numpy as np
-from tkinter import filedialog
-from tkinter import *
-import time
 
-root = Tk()
-root.update()
-file = filedialog.askopenfilename(initialdir = "/home/riderasg/amogh/8th Sem/major project/project/final",title = "Select file",filetypes = (("all files","*.*"),("webm files","*.webm"),("mp4 files","*.mp4")))
-root.destroy()
-
-rgb = cv2.VideoCapture(file)
+rgb = cv2.VideoCapture(0)
 facec = cv2.CascadeClassifier('./models/haarcascade_frontalface_default.xml')
 font = cv2.FONT_HERSHEY_SIMPLEX
 
@@ -28,26 +20,23 @@ def __get_data__():
     return faces, fr, gray
 
 def start_app(cnn):
+    
     while True:
         faces, fr, gray_fr = __get_data__()
         for (x, y, w, h) in faces:
             fc = gray_fr[y:y+h, x:x+w]
-            
             roi = cv2.resize(fc, (48, 48))
             pred, prob = cnn.predict_emotion(roi[np.newaxis, :, :, np.newaxis])
             text = "%s , %.2f"%(pred,prob * 100)
             cv2.putText(fr, text, (x, y), font, 1, (255, 255, 0), 2)
             cv2.rectangle(fr,(x,y),(x+w,y+h),(255,0,0),2)
 
-            
-
         if cv2.waitKey(1) == 27:
             break
-        imS = cv2.resize(fr,(500,500)) 
-        cv2.imshow('Video', imS)
-        time.sleep(0.1)
+        cv2.imshow('WebCam', fr)
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     model = FacialExpressionModel("./models/model.json", "./models/model.h5")
     start_app(model)
+
